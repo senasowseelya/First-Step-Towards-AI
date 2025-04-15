9dc8068727b29be64735e1007e9550d5030ab0d9
mohithvishnumolakala <mohithvishnumolakala@gmail.com>
2025-04-15 19:40:39 +0530

    introduction to python

---
+++ intro.py
@@ -0,0 +1,12 @@
+'''
+Python is an most programming language
+used in:-
+web development
+data science
+machine learning
+and automation
+
+python is
+1.easy to learn
+2.large eco-system(many libraries)
+'''
