d659f1662d80c99e3259f5077077d29ac30bccfa
sv185275_ncrvoyix <sv185275@ncr.com>
2025-04-15 08:31:07 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,35 @@
+"""
+To execute the program or block of code multiple times, we use while loop
+This while loop executes if the condition is true
+if condition is false, it comes out of loop
+
+We use while loop  to execute until a condition becomes false
+"""
+
+
+units = 'seconds'
+units_count= 24*60*60
+
+def days_to_units(days):
+        return f"{days} days are {units_count} {units}"
+
+def validate_execute():
+    try:                             # instead of if, we used try as try covers all kinds of exceptions or unknown issues
+        num = int(num_of_days)
+        if num > 0:
+            return_val = days_to_units(int(num_of_days))
+            return return_val
+        elif num == 0:
+            return "You entered 0, enter a valid number"
+        else:
+            return "You entered negative, enter a valid number"
+
+    except ValueError:                 # if there is any error in try block, except/ catch block will be called
+        return "input is not an integer"
+
+
+num_of_days = ''
+while num_of_days != "exit":      # this while continues until exit is typed because exit != exit is false, it terminates
+    num_of_days = input("Enter days\n")
+    print(validate_execute())
+
