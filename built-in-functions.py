25f40cf8354ad0aea7a7d93a3a85b9f9d1160c57
sena <senasowseelya2001@gmail.com>
2025-04-27 15:27:15 +0530

    built-in vs user-defined functions

---
+++ built-in-functions.py
@@ -0,0 +1,13 @@
+print("test")
+int("23")
+set([1,3,4])
+
+
+# all if these are python  built in functions where they take some input, performs actions and return an output value
+# we can also have user-defined function like this
+
+
+def days_to_units(days):
+    return f"{days} days are 12123"
+
+days_to_units(2)
