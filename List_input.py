f753d721893bbe34053855fd4455ceb92f722b46
sv185275_ncrvoyix <sv185275@ncr.com>
2025-04-27 12:38:14 +0530

    list opearations

---
+++ List_input.py
@@ -0,0 +1,11 @@
+
+days=[1,2,3]
+print(days)
+days.append(4)  #adds an element to end of array
+print(days)
+days.remove(2)  # removes the first occurence
+print(days)
+print(len(days))  # length of list
+
+test= [1,"sample",3]  # list can also have multiple data types in a one variable
+print(test)
