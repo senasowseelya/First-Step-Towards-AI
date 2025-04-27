a2e6d3432af097b0ab981f79c5e62f2567370e1c
sena <senasowseelya2001@gmail.com>
2025-04-27 18:57:03 +0530

    spreadsheet, counting rows and iterating cells

---
+++ spreadsheet_exer_1.py
@@ -0,0 +1,18 @@
+from openpyxl.reader.excel import load_workbook
+
+inventory_workbook = load_workbook("inventory.xlsx")
+products_list = inventory_workbook["Sheet1"]
+#print(products_list)   #<Worksheet "Sheet1">
+
+products_per_supplier={}
+
+for product_row in range(2,products_list.max_row+1):
+    #print(product_row)
+    supplier_name = products_list.cell(product_row,4).value  # because supplier name is at column 4
+    #print(supplier_name)
+    if supplier_name not in products_per_supplier:
+        products_per_supplier[supplier_name] = 1
+    else:
+        products_per_supplier[supplier_name] += 1
+
+print(products_per_supplier)
