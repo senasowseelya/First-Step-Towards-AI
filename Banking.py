78117f6c52043119d6b0aceed548dd4eaf26c861
mohithvishnumolakala <mohithvishnumolakala@gmail.com>
2025-05-15 20:53:50 +0530

    added new learned files

--- Banking.py
+++ Banking.py
@@ -1,32 +1,39 @@
-"""3 options:
-1.deposit
-2.withdraw
-3.check balane
-
-1. click>
-how much deposit u want to add
-and add it to total and show the balance
+def display_options():
+    print("Banking Options:\n 1.Deposit \n 2.Withdraw \n 3.Check Balance")
+    global user_input
+    user_input=input("enter which option you want to experience: ")
 
 
-2.>
-how much u want to withdraw
-sub from total and show balance
+def deposit():
+    deposit_val = int(input("how much money you want to deposit:"))
+    global total
+    total = total + deposit_val
+    check_balance()
 
-3> just showiung balance"""
+
+def withdraw():
+    withdraw_val = int(input("how much money you want to withdraw:"))
+    global total
+    total = total - withdraw_val
+    check_balance()
+
+
+def check_balance():
+    print(f"Your current Balance: {total}")
+
 
 total=1000
-print("Banking Options:\n 1.Deposit \n 2.Withdraw \n 3.Check Balance")
-user_input=int(input("enter which option you want to experience: "))
-if user_input==1:
-    deposit=int(input("how much money you want to deposit:"))
-    total=total+deposit
-    print(f"Your current Balance after deposit: {total}")
-elif user_input==2:
-    withdraw = int(input("how much money you want to withdraw:"))
-    total = total - withdraw
-    print(f"Your current Balance after withdrawal: {total}")
-elif user_input==3:
-    print(f"Your current Balance: {total}")
-else:
-    print("you entered a invaliod number")
+user_input=0
+display_options()
+while user_input!="exit":
+    if user_input=="1":
+        deposit()
+    elif user_input=="2":
+        withdraw()
+    elif user_input=="3":
+        check_balance()
+    else:
+        print("you entered a invalid number")
+    display_options()
+
 
